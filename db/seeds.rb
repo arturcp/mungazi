3ee46a29399d674d6f62f7fabe13d4fe1e491f40
Artur <artur.prado@gmail.com>
2018-08-04 16:35:46 -0300

    Try to upload file to carrierwave on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,11 @@ puts 'done'
 
 
 puts 'creating articles...'
-Article.create!(title: 'Agradecimentos à Cassia', short_title: 'Agradecimentos à Cassia', image_url: File.open(File.join(Rails.root, 'db', 'seed_images', 'item1.jpg')), content: 'Hello World')
+
+file = File.open(File.join(Rails.root, 'db', 'seed_images', 'item1.jpg'))
+uploader = ImageUploader.new
+uploader.store!(file)
+Article.create!(title: 'Agradecimentos à Cassia', short_title: 'Agradecimentos à Cassia', image_url: file, content: 'Hello World')
 Article.create!(title: 'Erick Guanho', short_title: 'Erick Guanho', image_url: File.open(File.join(Rails.root, 'db', 'seed_images', 'item2.jpg')), content: 'lorem ipsum')
 Article.create!(title: 'Luciana', short_title: 'Luciana', image_url: File.open(File.join(Rails.root, 'db', 'seed_images', 'item3.jpg')), content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.')
 puts 'done'
