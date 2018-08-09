13441fae2929747c7d2da769ca39f8acc32c9842
Artur <artur.prado@gmail.com>
2018-08-08 22:31:31 -0300

    List next events on home and improve layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.order('date desc').take(3)
+    @events = Event.where('created_at >= ?', Date.current).order('date desc')
+    @events = Event.order('date desc').take(3) unless @events
+
     @videos = Video.order('created_at desc').take(3)
     @articles = Article.order('created_at desc').take(6)
   end
